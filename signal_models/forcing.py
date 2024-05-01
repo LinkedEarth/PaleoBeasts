b4f58c523494a06c28887cb2573977598d78ac6b
Jordan Landers <jordan.landers+dev@gmail.com>
2024-05-01 15:23:40 -0800

    removed derivative

--- signal_models/forcing.py
+++ signal_models/forcing.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 class Forcing:
-    def __init__(self, data, time=None, derivative=None, params=None):
+    def __init__(self, data, time=None, params=None):
         """
         Initialize the Forcing class.
         --forcing from vector untested!!!!
@@ -22,7 +22,6 @@ class Forcing:
         self.data = data
         self.time = time
         self.params = params if params is not None else {}
-        self.derivative = derivative
 
     def get_forcing(self, t):
         """
@@ -34,20 +33,3 @@ class Forcing:
             idx = int(t)  # TODO Assuming t is an index; interpolate if not
             return self.data[idx]
 
-    def get_derivative(self, t):
-        """
-        Get the derivative of the forcing at time t.
-        """
-        if self.derivative is not None:
-            return self.derivative(t)
-        elif isinstance(self.data, np.ndarray):
-            # Calculate numerical derivative if `derivative` is not provided and `data` is an array
-            if not hasattr(self, 'numeric_derivative'):
-                if self.time is not None:
-                    self.numeric_derivative = np.gradient(self.data, self.time)
-                else:
-                    self.numeric_derivative = np.gradient(self.data)  # Assumes uniform spacing of 1
-            idx = int(t)
-            return self.numeric_derivative[idx]
-        else:
-            raise ValueError("No method for derivative calculation provided.")
