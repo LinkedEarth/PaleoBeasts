796a08a8c83bf36292e4429954c4819f270fe9bb
Jordan Landers <jordan.landers+dev@gmail.com>
2024-05-10 14:16:50 -0700

    add time_util option

--- paleobeasts/signal_models/g24.py
+++ paleobeasts/signal_models/g24.py
@@ -84,9 +84,11 @@ class Model3(PBModel):
         v = x[0]  # int(self.state_variables[-1][0])
         if isinstance(v, np.ndarray):
             v = v[-1]
+        if v<0:
+            v=0
         k = int(self.state_variables['k'][-1])
-        f = self.forcing.get_forcing(t)
-        dfdt = self.calc_dfdt(t)
+        f = self.forcing.get_forcing(self.time_util(t))
+        dfdt = self.calc_dfdt(self.time_util(t))
 
         vc = self.calc_vc(t)
 
@@ -100,6 +102,7 @@ class Model3(PBModel):
 
         new_row = np.array([(v, k)], dtype=self.dtypes)
         self.state_variables = np.concatenate([self.state_variables, new_row], axis=0)
+        self.time.append(t)
 
         return [dvdt]
 
